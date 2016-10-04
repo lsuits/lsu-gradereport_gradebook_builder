8536f373773b6983930e6158c20f015e10bdb678
Chad Mazilly <chadmazilly@gmail.com>
2016-10-04 15:54:22 -0500

    fix some layout issues

--- index.php
+++ index.php
@@ -1,29 +1,50 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The gradebook builder report
+ *
+ * @package   gradereport_gradebook_builder
+ * @copyright 
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once '../../../config.php';
 require_once $CFG->libdir . '/gradelib.php';
 require_once $CFG->dirroot . '/grade/lib.php';
 require_once $CFG->dirroot . '/grade/report/gradebook_builder/lib.php';
 
-$PAGE->requires->jquery();
-$PAGE->requires->js('/grade/report/gradebook_builder/app.js');
-
 $courseid = required_param('id', PARAM_INT);
 $template_id = optional_param('template', null, PARAM_INT);
 
-$c_param = array('id' => $courseid);
-
-$PAGE->set_url(new moodle_url('/grade/report/gradebook_builder/index.php', $c_param));
-
+$PAGE->requires->jquery();
+$PAGE->requires->js('/grade/report/gradebook_builder/app.js');
 $PAGE->requires->css('/grade/report/gradebook_builder/grid.css');
 $PAGE->requires->css('/grade/report/gradebook_builder/app.css');
 
-$course = $DB->get_record('course', $c_param, '*', MUST_EXIST);
+$PAGE->set_url(new moodle_url('/grade/report/gradebook_builder/index.php', array('id' => $courseid)));
 
+/// basic access checks
+if (!$course = $DB->get_record('course', array('id' => $courseid))) {
+    print_error('nocourseid');
+}
 require_login($course);
+$PAGE->set_pagelayout('report');
 
 $context = context_course::instance($course->id);
-
 require_capability('gradereport/gradebook_builder:view', $context);
 require_capability('moodle/grade:edit', $context);
 
@@ -33,7 +54,11 @@ $gpr = new grade_plugin_return(array(
     'courseid' => $courseid
 ));
 
-$reportname = get_string('pluginname', 'gradereport_gradebook_builder');
+/// last selected report session tracking
+if (!isset($USER->grade_last_report)) {
+    $USER->grade_last_report = array();
+}
+$USER->grade_last_report[$course->id] = 'gradebook_builder';
 
 $template = $DB->get_record('gradereport_builder_template', array(
     'id' => $template_id
@@ -41,11 +66,10 @@ $template = $DB->get_record('gradereport_builder_template', array(
 
 $report = new grade_report_gradebook_builder($courseid, $gpr, $context, $template);
 
-if ($data = data_submitted() and !$report::is_gradebook_established($courseid)) {
-    $report->process_data($data);
-}
+if ($data = data_submitted() and !$report::is_gradebook_established($courseid)) {}
 
-print_grade_page_head($course->id, 'report', 'gradebook_builder', $reportname);
+print_grade_page_head($course->id, 'report', 'gradebook_builder', get_string('pluginname', 'gradereport_gradebook_builder'));
+groups_print_course_menu($course, $gpr->get_return_url('index.php?id='.$courseid, $template_id ? ['template' => $template_id] : ''));
 
 if ($report::is_gradebook_established($courseid)) {
     $gradebook_url = new moodle_url('/grade/edit/tree/index.php', array(
